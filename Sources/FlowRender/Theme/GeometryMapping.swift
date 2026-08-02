4e8a20ba0af7ad3a4c2d38254e774141005b52ef
Ayush Mishra Cognitain <ayush.mishra@cognitain.in>
2026-08-03 03:32:23 +0530

    Clarify GeometryMapping doc comment.

--- Sources/FlowRender/Theme/GeometryMapping.swift
+++ Sources/FlowRender/Theme/GeometryMapping.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import FlowCore
 
 public extension EdgeInsetsData {
+    /// Maps the backend's four sided inset atom to SwiftUI's EdgeInsets.
     var edgeInsets: EdgeInsets {
         EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
     }
